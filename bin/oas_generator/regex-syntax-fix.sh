dee6358d7e1e120295a386f6646ec427d73f9cf1
Jason Frey <fryguy9@gmail.com>
2022-06-23 13:36:39 -0400

    Update patching scripts to fail on error and support MacOS

--- bin/oas_generator/regex-syntax-fix.sh
+++ bin/oas_generator/regex-syntax-fix.sh
@@ -1,5 +1,12 @@
-find . -name "*.rb" -exec sed -i -e '/Regexp.new/ s|\\\\/|\\/|g' {} +
-find . -name "*.rb" -exec sed -i -e 's|\\xFF|\\u00FF|g' {} +
-find . -name "*.rb" -exec sed -i -e 's|#\[\\\]|#\\\[\\\]|g' {} +
-find . -name "*.rb" -exec sed -i -e 's|a-zA-Z0-9-|a-zA-Z0-9\\-|g' {} +
+#!/bin/bash
 
+# MacOS uses BSD sed which has a different CLI, so on Mac prefer gnu-sed.
+#   gnu-sed can be installed on MacOS with `brew install gnu-sed`
+cmd="$([[ "$OSTYPE" == "darwin"* ]] && echo "gsed" || echo "sed")"
+
+set -e
+
+find . -name "*.rb" -exec $cmd -i -e '/Regexp.new/ s|\\\\/|\\/|g' {} +
+find . -name "*.rb" -exec $cmd -i -e 's|\\xFF|\\u00FF|g' {} +
+find . -name "*.rb" -exec $cmd -i -e 's|#\[\\\]|#\\\[\\\]|g' {} +
+find . -name "*.rb" -exec $cmd -i -e 's|a-zA-Z0-9-|a-zA-Z0-9\\-|g' {} +
