18cc77c0ed28a1292ae3fbab47dde59b1151a86c
Jason Frey <fryguy9@gmail.com>
2022-06-27 03:32:41 -0400

    Add bin/build script to simplify regenerating the gem (#15)
    * Update patching scripts to fail on error and support MacOS
    
    * Add bin/build script to simplify regenerating the gem

--- bin/oas_generator/replace-discriminator-names.sh
+++ bin/oas_generator/replace-discriminator-names.sh
@@ -1,8 +1,12 @@
-find lib/intersight_client/models/ -name "*.rb" -exec sed -i '/def openapi_discriminator_name$/{n;s/object_type/ObjectType/}' {} +
+#!/bin/bash
 
-find lib/intersight_client/models/ -name "*.rb" -exec sed -i '/def self.openapi_discriminator_name$/{n;s/object_type/ObjectType/}' {} +
+# MacOS uses BSD sed which has a different CLI, so on Mac prefer gnu-sed.
+#   gnu-sed can be installed on MacOS with `brew install gnu-sed`
+cmd="$([[ "$OSTYPE" == "darwin"* ]] && echo "gsed" || echo "sed")"
 
-find lib/intersight_client/models/ -name "*.rb" -exec sed -i '/def openapi_discriminator_name$/{n;s/class_id/ClassId/}' {} +
+set -e
 
-find lib/intersight_client/models/ -name "*.rb" -exec sed -i '/def self.openapi_discriminator_name$/{n;s/class_id/ClassId/}' {} +
-
+find lib/intersight_client/models/ -name "*.rb" -exec $cmd -i '/def openapi_discriminator_name$/{n;s/object_type/ObjectType/}' {} +
+find lib/intersight_client/models/ -name "*.rb" -exec $cmd -i '/def self.openapi_discriminator_name$/{n;s/object_type/ObjectType/}' {} +
+find lib/intersight_client/models/ -name "*.rb" -exec $cmd -i '/def openapi_discriminator_name$/{n;s/class_id/ClassId/}' {} +
+find lib/intersight_client/models/ -name "*.rb" -exec $cmd -i '/def self.openapi_discriminator_name$/{n;s/class_id/ClassId/}' {} +
